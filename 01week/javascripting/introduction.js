7fc32131c942d17d7d579de6d52a068c612bf9ce
hampsonn <hampsonn@sualumni.net>
2018-04-20 14:37:31 -0500

    conditionals corrected

--- 01week/javascripting/introduction.js
+++ 01week/javascripting/introduction.js
@@ -44,30 +44,26 @@ console.log(addThese(4,900));
 // This will succeed only if two things are true
 
 function pickyAdder (aNumber, aString) {
-  if (typeof(aNumber) === "number" && typeof(aString) === "string") {
-    console.log("I would like to buy " + aNumber + " " + aString + "s");
-  } else {
-    console.log("Please input a number and a string to make this purchase");
+  if (aNumber && aString) {
+    return "Success";
   }
 }
-pickyAdder("six hundred", "balloon")
 pickyAdder(12, "rattlesnake");
 
-// This will succeed as long as one of its two conditions are met
+// This will succeed as long as one of its two conditions is true
 
 function lessPicky (firstNum, secNum) {
-  if (firstNum > 0 || secNum > 0) {
-    console.log(firstNum + secNum);
-  } else console.log("Please choose at least one positive number, as it is more fun");
+  if (firstNum || secNum) {
+    return "Success";
+  }
 }
-lessPicky(-4, 0);
-lessPicky(-2, 3);
+lessPicky(-2, false);
 
-// This will only succeed if none of its conditions are true
+// This will only succeed if none of its conditions evaluate true
 
-function grouchy (aNewString) {
-  if (aNewString.length > 10 || typeof(aNewString) != "string") {
-  } else console.log("ty for being brief")
+function grouchy (aNewString, aNewNull) {
+  if (!aNewString && !aNewNull) {
+    return "Success";
+  }
 }
-grouchy("this function is rude");
-grouchy("okay");
+grouchy("", null);
