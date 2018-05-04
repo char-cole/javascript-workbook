2b9f34b495fb14277c6299051ed4f8c2827867ac
hampsonn <hampsonn@sualumni.net>
2018-05-03 19:07:49 -0500

    first 5

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+use strict;
+
+const printListOfOlympians = (array1) => {
+  array1.forEach(function(item) {
+    console.log(item);
+  })
+}
+
+printListOfOlympians(["Paola", "Fred Rogers", "Jimbo", "Ron", "Cruella"]);
