32145f4e9a580d37fb3f36121d36c95ca82c4b5e
hampsonn <hampsonn@sualumni.net>
2018-05-16 08:08:18 -0500

    last 3 questions

--- 04week/loop.js
+++ 04week/loop.js
@@ -36,3 +36,14 @@ do {
   countr++;
   console.log(countr);
 } while (countr < 1000);
+
+// When is a for loop better than a while loop? How is the readability of the code affected?
+  // A FL might be better if you know you just need to iterate a certain number of times vs an abstract condition
+  // A FL can be more concise than a WL and can even be just one line with no braces.
+
+// What is the difference between a for loop and a for...in loop?
+  // A FL can do anything, but a F...I is made for looping through an object's keys
+
+// What is the difference between a while loop and a do...while loop?
+  // a WL will check its condition before attempting to execute,
+  // but a D...W will execute one time before checking its condition
