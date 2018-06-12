8cbfffcc243f4fde2922f60f6637808c143eb2b9
hampsonn <hampsonn@sualumni.net>
2018-06-12 16:52:25 -0500

    gallery with lightbox

--- photogal/src/App.js
+++ photogal/src/App.js
@@ -1,32 +1,73 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import Lightbox from 'react-images';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 import './Normalize.css';
 import './App.css';
 
-// import Images from './ImagesFromAPI';
-// import Gallery from './Gallery';
+const urls = [
+  {source:"https://i.imgur.com/xnXcHFq.png", alt:"Ice in Bloom", key:0},
+  {source:"https://i.imgur.com/b9Rkh8V.png", alt:"Umi at Rest", key:1},
+  {source:"https://i.imgur.com/ctM8fcC.png", alt:"Sappho's Excursion", key:2},
+  {source:"https://i.imgur.com/LqLWlMM.png", alt:"Frodo at Play", key:3},
+  {source:"https://i.imgur.com/8pemaDK.png", alt:"The Microbial Host", key:4},
+  {source:"https://i.imgur.com/5RLkNkN.png", alt:"The Wary Anole", key:5},
+  {source:"https://i.imgur.com/KKgfLZa.png", alt:"River Hunter", key:6},
+  {source:"https://i.imgur.com/ocQIxpj.png", alt:"Crisp Leaves", key:7},
+  {source:"https://i.imgur.com/Vk4wrtv.png", alt:"Local Wasp", key:8},
+  {source:"https://i.imgur.com/EFEKzqd.png", alt:"Spider over Rotten Leaves", key:9},
+];
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+      isLoaded: false,
+    };
+  }
 
   renderImage(imageURL) {
     return (
       <div className="imageHolder">
-        <img src={imageURL.source} alt={imageURL.alt} />
+        <img src={imageURL.source} alt={imageURL.alt} onClick={() => this.setState({ photoIndex: imageURL.key, isOpen: true, })} />
       </div>
     );
   }
 
   render() {
+    const { photoIndex, isOpen } = this.state;
+
     return (
       <div className="imageGallery">
         <div className="displayImages grid-container">
-          {this.props.imageURLs.map(imageURL => this.renderImage(imageURL))}
+          {urls.map(imageURL => this.renderImage(imageURL))}
         </div>
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={urls[photoIndex].source}
+            imageCaption={urls[photoIndex].alt}
+            // Have not recovered image change functionality. Stuck looking at one at a time.
+            // nextSrc={urls[(photoIndex + 1)].source % urls.length}
+            // prevSrc={urls[(photoIndex - 1)].source % urls.length}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + urls.length - 1) % urls.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % urls.length,
+              })
+            }
+          />
+        )}
       </div>
-    )
+    );
   }
 }
-
-export default App;
--- photogal/src/index.js
+++ photogal/src/index.js
@@ -4,18 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const urls = [
-  {source:"https://i.imgur.com/xnXcHFq.png", alt:"Ice in Bloom"},
-  {source:"https://i.imgur.com/b9Rkh8V.png", alt:"Umi at Rest"},
-  {source:"https://i.imgur.com/ctM8fcC.png", alt:"Sappho's Excursion"},
-  {source:"https://i.imgur.com/LqLWlMM.png", alt:"Frodo at Play"},
-  {source:"https://i.imgur.com/8pemaDK.png", alt:"The Microbial Host"},
-  {source:"https://i.imgur.com/5RLkNkN.png", alt:"The Wary Anole"},
-  {source:"https://i.imgur.com/KKgfLZa.png", alt:"River Hunter"},
-  {source:"https://i.imgur.com/ocQIxpj.png", alt:"Inside the Cup"},
-  {source:"https://i.imgur.com/Vk4wrtv.png", alt:"Local Wasp"},
-  {source:"https://i.imgur.com/EFEKzqd.png", alt:"Spider over Rotten Leaves"},
-];
-
-ReactDOM.render(<App imageURLs={urls}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
